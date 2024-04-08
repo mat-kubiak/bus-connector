59468d9597b8236d9f90a2109315680177a4c864
Mateusz Kubiak <matkubiak888@gmail.com>
2024-04-08 04:25:15 +0100

    logger support

--- src/main/java/com/github/mat_kubiak/tqs/bus_connector/BusConnectorApplication.java
+++ src/main/java/com/github/mat_kubiak/tqs/bus_connector/BusConnectorApplication.java
@@ -2,11 +2,15 @@ package com.github.mat_kubiak.tqs.bus_connector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class BusConnectorApplication {
+	private static final Logger logger = LoggerFactory.getLogger(BusConnectorApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Application started!");
 		SpringApplication.run(BusConnectorApplication.class, args);
 	}
 
