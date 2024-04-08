7cb406cf38c7144fa73aec2fe50f850d3f375af6
Mateusz Kubiak <matkubiak888@gmail.com>
2024-04-08 20:01:22 +0100

    feat: origin and destination city selectors

--- src/main/java/com/github/mat_kubiak/tqs/bus_connector/controllers/HomePageController.java
+++ src/main/java/com/github/mat_kubiak/tqs/bus_connector/controllers/HomePageController.java
@@ -13,24 +13,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.github.mat_kubiak.tqs.bus_connector.data.City;
 
 @Controller
-public class HomeController {
+public class HomePageController {
     private static final Logger logger = LoggerFactory.getLogger(BusConnectorApplication.class);
     private final CityManagerService tripService;
 
-    public HomeController(CityManagerService cityManagerService) {
+    public HomePageController(CityManagerService cityManagerService) {
         this.tripService = cityManagerService;
     }
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("cities", tripService.getAllCities());
-        return "index"; // Return the name of the Thymeleaf template
+        return "index";
     }
 
-    @RequestMapping(value="/add-city", method = RequestMethod.POST)
-    public String handleAction() {
-        logger.info("A city has been added!");
-        tripService.save(new City("Thyme"));
-        return "redirect:/";
-    }
+//    @RequestMapping(value="/add-city", method = RequestMethod.POST)
+//    public String handleSearch() {
+//        logger.info("A city has been added!");
+//        tripService.save(new City("Thyme"));
+//        return "redirect:/";
+//    }
 }
