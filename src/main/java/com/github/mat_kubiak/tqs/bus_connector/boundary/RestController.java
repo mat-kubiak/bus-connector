db7c00035fc5e1ff967599ddb00a5407a3b6e019
Mateusz Kubiak <matkubiak888@gmail.com>
2024-04-09 07:31:06 +0100

    refactor: http response entities in rest controller

--- src/main/java/com/github/mat_kubiak/tqs/bus_connector/boundary/RestController.java
+++ src/main/java/com/github/mat_kubiak/tqs/bus_connector/boundary/RestController.java
@@ -6,11 +6,15 @@ import com.github.mat_kubiak.tqs.bus_connector.data.Ticket;
 import com.github.mat_kubiak.tqs.bus_connector.data.Trip;
 import com.github.mat_kubiak.tqs.bus_connector.service.ExchangeRateService;
 import com.github.mat_kubiak.tqs.bus_connector.service.ManagerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
@@ -25,46 +29,64 @@ public class RestController {
     }
 
     @GetMapping(path = "/cities",  produces = "application/json")
-    public List<City> getAllCities() {
-        return managerService.getAllCities();
+    public ResponseEntity<List<City>> getAllCities() {
+        List<City> cities = managerService.getAllCities();
+        if (cities.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(cities, HttpStatus.OK);
     }
 
     @GetMapping(path = "/trips",  produces = "application/json")
-    public List<Trip> getTripsInfo(@RequestParam(required = true) Long from,
+    public ResponseEntity<List<Trip>> getTripsInfo(@RequestParam(required = true) Long from,
                                    @RequestParam(required = true) Long to,
                                    @RequestParam(required = true) Date date) {
 
         City fromCity = managerService.getCity(from);
         City toCity = managerService.getCity(to);
         if (fromCity == null || toCity == null) {
-            return Collections.emptyList();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
 
-        return managerService.getTrips(fromCity, toCity, date);
+        List<Trip> trips = managerService.getTrips(fromCity, toCity, date);
+        if (trips.isEmpty()) {
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(trips, HttpStatus.OK);
     }
 
     @GetMapping(path = "/exchange-rates", produces = "application/json")
-    public ExchangeRateResponse getExchangeRates() {
-        return rateService.getExchangeRates();
+    public ResponseEntity<ExchangeRateResponse> getExchangeRates() {
+        ExchangeRateResponse response = rateService.getExchangeRates();
+        if (response == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping(path = "/ticket", produces = "application/json")
-    public Ticket getTicket(@RequestParam(required = true) Long id) {
-        return new Ticket();
+    public ResponseEntity<Ticket> getTicket(@RequestParam(required = true) Long id) {
+        Optional<Ticket> ticketOpt = managerService.getTicket(id);
+        if (ticketOpt.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(ticketOpt.get(), HttpStatus.OK);
     }
 
     @PostMapping(path = "/ticket", produces = "application/json")
-    public Long reserveTicket(@RequestParam(required = true) Long tripId,
+    public ResponseEntity<Ticket> reserveTicket(@RequestParam(required = true) Long tripId,
                               @RequestParam(required = true) Date date,
                               @RequestParam(required = true) String firstName,
                               @RequestParam(required = true) String lastName) {
 
         Trip trip = managerService.getTrip(tripId);
         if (trip == null) {
-
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
 
         Ticket ticket = managerService.bookTicket(trip, date, firstName, lastName);
-        return ticket.getTicketId();
+        return new ResponseEntity<>(ticket, HttpStatus.OK);
     }
 }
