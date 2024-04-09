48dc11c491eb99872de76fbc365a0f7179c79f8d
Mateusz Kubiak <matkubiak888@gmail.com>
2024-04-09 22:11:03 +0100

    cacheable exchanges

--- src/main/java/com/github/mat_kubiak/tqs/bus_connector/BusConnectorApplication.java
+++ src/main/java/com/github/mat_kubiak/tqs/bus_connector/BusConnectorApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
+@EnableScheduling
+@EnableJpaRepositories
 public class BusConnectorApplication {
 	private static final Logger logger = LoggerFactory.getLogger(BusConnectorApplication.class);
 
--- src/main/java/com/github/mat_kubiak/tqs/bus_connector/service/ExchangeRateService.java
+++ src/main/java/com/github/mat_kubiak/tqs/bus_connector/service/ExchangeRateService.java
@@ -1,13 +1,20 @@
 package com.github.mat_kubiak.tqs.bus_connector.service;
 
+import com.github.mat_kubiak.tqs.bus_connector.BusConnectorApplication;
 import com.github.mat_kubiak.tqs.bus_connector.data.ExchangeRateResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ExchangeRateService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BusConnectorApplication.class);
 
     @Value("${exchange.rate.api.url}")
     private String apiUrl;
@@ -18,8 +25,14 @@ public class ExchangeRateService {
         this.restTemplate = restTemplate;
     }
 
-    @Cacheable("exchangeRates")
+    @Cacheable(cacheNames = "exchangeRates")
     public ExchangeRateResponse getExchangeRates() {
         return restTemplate.getForObject(apiUrl, ExchangeRateResponse.class);
+    }
+
+    @Scheduled(fixedRate = 100000)
+    @CacheEvict(cacheNames = "exchangeRates", allEntries = true)
+    public void emptyCurrenciesCache() {
+        logger.info("Clearing exchange rate cache");
     }
 }
